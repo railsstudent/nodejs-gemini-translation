fd0d345193f3bdc3a73bf9c619d1457b7f86058e
Connie Leung <cleung334@gmail.com>
2024-06-05 20:29:23 +0800

    refactor: remove filter() because only one language

--- index.ts
+++ index.ts
@@ -1,31 +1,29 @@
 import { generativeModel } from './model';
 
+const source = 'en';
+const target = process.env.TARGET || 'ja';
+
 async function main() {
     const samples = [
         'Good morning',
         'Hello, how are you today?',
-        'How much does 3 apples and 4 oranges cost?'
+        'How much does 3 apples, 5 pineapples and 4 oranges cost?',
+        'My favorite hobby is riding bicycle.',
     ]
-
-    const language = {
-        source: 'en',
-        target: 'zh-Hant',
-    }
 
     const arrTranslations: Record<string, string>[] = [];
 
     for (let str of samples) {
         const generatedContents = await generativeModel.generateContent({
             systemInstruction: `You are a translation expert that can translate between source and target languages.
-                Return the source and translation only.  If you dont know the translation,  then return 
-                "UNKNOWN TRANSLATION".
-                Do not include the source language code and the source text in the response.
-                The response is target_language_code:translation\n
+                If you dont know the translation, then return "UNKNOWN TRANSLATION".
+                For numbers, please use words to represent instead of the arabic values.
+                The response is target_language_code|translation.
             `,
             contents: [
                 {
                     role: 'user',
-                    parts: [{ text: `${language.source}:${str} ${language.target}:` }],
+                    parts: [{ text: `${source}:${str} ${target}:` }],
                 }
             ],
         });
@@ -33,18 +31,14 @@ async function main() {
         const candidates = generatedContents.response.candidates || [];
         for (const candidate of candidates) {
             const translations: Record<string, string> = {
-                [language.source]: str
+                [source]: str
             };
 
             (candidate.content.parts || []).forEach((part) => {
                 console.log('part', part);
-                const lines = (part.text || '').split('\n')
-                    .filter((line) => line !== '')
-                    .filter((line) => line.trim());
-                lines.forEach((line) => {
-                    const [targetLanguage, text] = line.split(':');    
-                    translations[targetLanguage] = text.trim()
-                })
+                const line = (part.text || '').trim();
+                const [targetLanguage, text = ''] = line.split('|');    
+                translations[targetLanguage] = text;
             });
 
             arrTranslations.push(translations);
